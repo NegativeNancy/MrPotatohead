3d35f6f8bdbc7647d58d6f2324579331ea557483
Negative Nancy <ivodenhertog@gmail.com>
2018-04-13 17:26:16 +0200

    Version 1 of the app!

--- app/src/main/java/com/example/ivodenhertog/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/ivodenhertog/mrpotatohead/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ivodenhertog.mrpotatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkClicked(View v) {
+        CheckBox checkbox = (CheckBox) v;
+        String checkName = checkbox.getText().toString();
+        int imageId = getResources().getIdentifier(checkName, "id", getPackageName());
+        if (checkbox.isChecked()){
+            ImageView image = findViewById(imageId);
+            if (image.getVisibility() == View.INVISIBLE) {
+                image.setVisibility(View.VISIBLE);
+            }
+        }
+        else {
+            ImageView image = findViewById(imageId);
+            image.setVisibility(View.INVISIBLE);
+        }
     }
 }
